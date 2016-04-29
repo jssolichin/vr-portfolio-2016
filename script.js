f1eacf5778a119330e26e99eb4483a7e779c749d
Jonathan Solichin <jssolichin@gmail.com>
2016-04-28 21:23:26 -0700

    Add logo, children gallery.

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 AFRAME.registerComponent('item', {
 	schema: {
-		offset: {default: '0 2 0' }
+		offset: {default: '0 4 -1.5' },
+		target: {default: '#camera'}
 	},
 	update: function (oldData) {
 		this.el.addEventListener('mouseenter', this.onEnter.bind(this));
 		this.el.addEventListener('mouseleave', this.onLeave.bind(this));
+		this.el.addEventListener('click', this.onClick.bind(this));
 	},
 	init: function (){
 		var el = this.el;
@@ -21,7 +23,7 @@ AFRAME.registerComponent('item', {
 		var entity = document.createElement('a-entity');
 		entity.setAttribute('position', this.data.offset);
 
-		sides.forEach((side) => {
+		sides.forEach((side,index) => {
 			var box = document.createElement('a-box');
 			box.setAttribute('mixin', 'border');
 			box.setAttribute('position', side.position);
@@ -48,5 +50,31 @@ AFRAME.registerComponent('item', {
 		this.data.borders.forEach((border) => {
 			border.emit('close')	
 		})
-	}
+	},
+	onClick: function (){
+		var el = this.el;
+		var camera = this.el.sceneEl.querySelectorAll(this.data.target)[0];
+		var childrenCenter = this.el.querySelectorAll('.childrenCenter')[0];
+
+		var vector = childrenCenter.object3D.matrixWorld.getPosition();
+		
+		camera.emit('moveHere', vector, false);
+	},
+});
+
+AFRAME.registerComponent('click-listener', {
+  init: function () {
+    var el = this.el;
+
+	el.addEventListener('moveHere', function (a,b,c) {
+
+			var animation = document.createElement('a-animation');
+			animation.setAttribute('attribute', 'position');
+			animation.setAttribute('to', a.detail.x + ' ' + a.detail.y + ' ' + a.detail.z);
+			animation.setAttribute('repeat', '0');
+
+			el.appendChild(animation);
+
+    });
+  }
 });
